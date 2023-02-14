e3996f0b6de0940b087cd7fca3a3badcc4c73193
Lenz Weber-Tronic <lenz@apollographql.com>
2023-02-14 16:29:25 +0100

    fixup ForwardRef

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import { Theme } from "./theme";
 import { createVariant, VariantProps, useRestyle, composeRestyleFunctions } from "@shopify/restyle";
-import { ComponentProps } from "react";
+import { ComponentProps, forwardRef } from "react";
 import { Pressable } from "./Pressable";
 import { Text } from "./Text";
 
@@ -11,23 +11,29 @@ const restyleFunctions = composeRestyleFunctions([
   // @ts-expect-error
   createVariant<Theme, "buttonVariants">({ themeKey: "buttonVariants" }),
 ]);
-
-export function Button({
-  label,
-  variant,
-  labelProps: textProps = {},
-  ...pressableProps
-}: { label: string } & RestyleProps & {
+type ButtonProps = { label: string } & RestyleProps & {
     labelProps?: ComponentProps<typeof Text>;
-  } & Omit<ComponentProps<typeof Pressable>, keyof RestyleProps>) {
+  } & Omit<ComponentProps<typeof Pressable>, keyof RestyleProps>;
+
+export const Button = forwardRef<typeof Pressable, ButtonProps>(function Button(
+  { label, variant, labelProps: textProps = {}, ...pressableProps },
+  ref
+) {
   // @ts-expect-error
   const pressable = useRestyle(restyleFunctions, { variant, style: pressableProps.style });
   // @ts-expect-error
   const text = useRestyle(restyleFunctions, { variant, style: textProps.style });
 
   return (
-    <Pressable flex={0} padding="s" borderRadius={10} {...pressableProps} style={pressable.style}>
+    <Pressable
+      flex={0}
+      padding="s"
+      borderRadius={10}
+      ref={ref}
+      {...pressableProps}
+      style={pressable.style}
+    >
       <Text children={label} {...textProps} style={text.style} />
     </Pressable>
   );
-}
+});
