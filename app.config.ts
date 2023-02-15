ac0ff151aaf9b1c71ede2d196e1c5a289b4edbfd
Lenz Weber-Tronic <lenz@apollographql.com>
2023-02-15 12:10:58 +0100

    enable source maps in `build.gradle`

---
+++ app.config.ts
@@ -0,0 +1,68 @@
+// @ts-check
+import {
+  withPlugins,
+  withGradleProperties,
+  ConfigPlugin,
+  withAppBuildGradle,
+} from "expo/config-plugins";
+import { ExpoConfig, ConfigContext } from "expo/config";
+
+export default ({ config }: ConfigContext): ExpoConfig => {
+  return withPlugins(
+    {
+      ...config,
+      name: "rn-apollo-client-testbed",
+      slug: "rn-apollo-client-testbed",
+      version: "1.0.0",
+      orientation: "portrait",
+      icon: "./assets/icon.png",
+      userInterfaceStyle: "light",
+      scheme: "rn-apollo-client-testbed",
+      splash: {
+        image: "./assets/splash.png",
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+      updates: {
+        fallbackToCacheTimeout: 0,
+      },
+      assetBundlePatterns: ["**/*"],
+      ios: {
+        supportsTablet: true,
+        bundleIdentifier: "apollo.prometheus.testbed",
+      },
+      android: {
+        adaptiveIcon: {
+          foregroundImage: "./assets/adaptive-icon.png",
+          backgroundColor: "#FFFFFF",
+        },
+        package: "apollo.prometheus.testbed",
+      },
+      web: {
+        favicon: "./assets/favicon.png",
+      },
+      extra: {
+        eas: {
+          projectId: "0c93a498-fc0d-4e45-9e08-36399405030b",
+        },
+      },
+      owner: "prometheus-web",
+      jsEngine: "hermes",
+    },
+    [withSourceMapsInDev]
+  );
+};
+
+const withSourceMapsInDev: ConfigPlugin = (config) =>
+  withAppBuildGradle(config, (config) => {
+    config.modResults.contents = config.modResults.contents.replace(
+      `
+project.ext.react = [`,
+      `
+project.ext.react = [
+    hermesFlagsRelease: ["-O", "-output-source-map"],
+    hermesFlagsDebug: ["-O", "-output-source-map"],
+`
+    );
+    return config;
+  });
